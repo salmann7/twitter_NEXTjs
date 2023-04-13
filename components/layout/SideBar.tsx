e9e8827e6ae0c1973175edf5188de23ef50a39ae
salman <salmann9977@gmail.com>
2023-04-14 03:04:37 +0530

    layout :- followBar component

---
+++ components/layout/SideBar.tsx
@@ -0,0 +1,5 @@
+import { signOut } from 'next-auth/react';
+import { BiLogOut } from 'react-icons/bi';
+import { BsHouseFill, BsBellFill } from 'react-icons/bs';
+import { FaUser } from 'react-icons/fa';
+
